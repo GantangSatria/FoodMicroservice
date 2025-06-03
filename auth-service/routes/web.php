33b74603dbb854391c9967770785af18c3d712a5
GantangSatria <gantang610@gmail.com>
2025-06-04 00:06:16 +0700

    feat(auth): adding auth service using jwt

--- auth-service/routes/web.php
+++ auth-service/routes/web.php
@@ -16,3 +16,9 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->post('/register', 'AuthController@register');
+$router->post('/login', 'AuthController@login');
+$router->post('/validate', 'AuthController@validateToken');
+$router->post('/logout', 'AuthController@logout');
+
